05da996aca0a4ed865b4d3fd618e6c1b177786c1
eidriB <sara.holmquist@gmail.com>
2022-02-25 10:43:55 +0100

    Created code library for eastereggs

---
+++ EasterEggDemo/EasterEggs.cs
@@ -0,0 +1,20 @@
+﻿namespace EasterEggDemo
+{
+    public class EasterEggs
+    {
+        public static bool IsEasterEgg(string input)
+        {
+            switch (input.ToLower())
+            {
+                case "xyzzy":
+                    Console.WriteLine("Nothing happens");
+                    return true;
+                case "what's 9+10":
+                    Console.WriteLine("21");
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
